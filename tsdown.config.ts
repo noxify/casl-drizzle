e963cefa9f6ffd80a39970f4fd518104a586d434
Marcus Reinhardt <webstone@gmail.com>
2026-02-10 22:44:05 +0100

    enable changesets

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsdown"
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  minify: false,
+  minify: true,
   dts: true,
   external: ["typescript"],
   format: ["esm"],
